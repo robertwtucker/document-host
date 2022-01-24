1689bfc337058b0dc39e1de1560fa764e60648e4
Robert Tucker <r.tucker@quadient.com>
2022-01-23 22:25:02 -0500

    refactor: switched ta distroless image

--- cmd/server/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-APP_ENV=${APP_ENV:-local}
-
-echo "Running entrypoint script in the '${APP_ENV}' environment..."
-CONFIG_FILE=/app/config/${APP_ENV}.yaml
-
-/app/server -config ${CONFIG_FILE}
