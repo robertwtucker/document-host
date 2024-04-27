094cce689b97da6b94829a7ca09902c50a5fa39e
Robert Tucker <robertwtucker@gmail.com>
2024-04-27 11:42:01 -0400

    refactor: moved auth to middleware

--- app/api/[version]/documents/route.ts
+++ app/api/[version]/documents/route.ts
@@ -4,7 +4,6 @@
  */
 
 import { NextRequest, NextResponse } from 'next/server'
-import { verifyToken } from '@/lib/jwt'
 import { insert } from '@/lib/api/document'
 import { shorten } from '@/lib/shortlink'
 import { logger } from '@/lib/logger'
@@ -18,40 +17,34 @@ export async function POST(req: NextRequest, context: { params: Params }) {
   const requestInfo = `${req.method} ${req.nextUrl.pathname}`
 
   if (version && version.match(new RegExp('^v[1-2]'))) {
-    const token = await verifyToken(req)
-    if (token && token.scope?.includes('create:documents')) {
-      const payload = await req.json()
-      const document = await insert(payload)
-      if (document) {
-        document.url = `${process.env.APP_URL}/${document.id}`
-        const shortened = await shorten(document.url)
-        if (shortened && shortened.shortlink) {
-          document.shortLink = shortened.shortlink
-        }
-
-        let versionedResponse = {}
-        if (version === 'v1') {
-          versionedResponse = document
-        } else {
-          versionedResponse = { document }
-        }
-
-        logger.info(requestInfo, { status: 201 })
-        return NextResponse.json(versionedResponse, {
-          headers: {
-            'Content-Type': 'application/json',
-            Location: document.url,
-          },
-          status: 201,
-        })
-      } else {
-        logger.error('Failed to insert document into the database')
-        logger.info(requestInfo, { status: 500 })
-        return new NextResponse(null, { status: 500 })
+    const payload = await req.json()
+    const document = await insert(payload)
+    if (document) {
+      document.url = `${process.env.APP_URL}/${document.id}`
+      const shortened = await shorten(document.url)
+      if (shortened && shortened.shortlink) {
+        document.shortLink = shortened.shortlink
       }
+
+      let versionedResponse = {}
+      if (version === 'v1') {
+        versionedResponse = document
+      } else {
+        versionedResponse = { document }
+      }
+
+      logger.info(requestInfo, { status: 201 })
+      return NextResponse.json(versionedResponse, {
+        headers: {
+          'Content-Type': 'application/json',
+          Location: document.url,
+        },
+        status: 201,
+      })
     } else {
-      logger.info(`${req.method} ${req.nextUrl.pathname}`, { status: 401 })
-      return new NextResponse(null, { status: 401 })
+      logger.error('Failed to insert document into the database')
+      logger.info(requestInfo, { status: 500 })
+      return new NextResponse(null, { status: 500 })
     }
   } else {
     logger.info(`${req.method} ${req.nextUrl.pathname}`, { status: 400 })
